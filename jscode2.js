5038714cf77214b8457d100823bf2fbcb45e64e3
kimarnaiz <61414451+kimarnaiz@users.noreply.github.com>
2020-02-29 03:25:02 +0800

    Update jscode2.js

--- jscode2.js
+++ jscode2.js
@@ -1,4 +1,18 @@
-const input = 123;
+//read input from user
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Please enter the number: ', (answer) => {
+  const input = answer;
+  calculateValidationNumber(input);
+  rl.close();
+});
+
+//const input = 44444;
 
 function calculateValidationNumber(input){
 	let result = Array.from(input.toString());
@@ -8,11 +22,15 @@ function calculateValidationNumber(input){
 		var sum = sum + parseInt(result[i]);
 	}
 	
-	console.log(sum);
-	
 	//return first digit
-	//let result2 = Array.from(input.toString());
+	// let result2 = Array.from(sum.toString());
+	// if (result2 > 1){
+		// calculateValidationNumber(sum);
+	// }
+	// else {
+		//console.log("Single digit number = " + sum[0]);
+	// }
 	
+	console.log(sum);	
 }
 
-calculateValidationNumber(input);
